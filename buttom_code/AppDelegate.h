147c3b59e6a5cf2a964cd6abfb0e3d7d90e7a155
apple <apple@appledeMacBook-Pro.local>
2015-04-23 11:28:14 +0800

    Initial Commit

---
+++ buttom_code/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buttom_code
+//
+//  Created by apple on 15/4/23.
+//  Copyright (c) 2015年 worldelite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ buttom_code/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  buttom_code
+//
+//  Created by apple on 15/4/23.
+//  Copyright (c) 2015年 worldelite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
